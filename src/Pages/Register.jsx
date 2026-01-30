f90a3c5197178d46d538e1c24dc371b530f950cf
Ubair <113605539+Blue-pill-786@users.noreply.github.com>
2026-01-30 21:17:27 +0530

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,40 +1,62 @@
 import React from "react";
-import Add from "../img/addAvatar.png"
-import { useValue } from "../context/itemContext";
 import { Link } from "react-router-dom";
-import bleeding  from '../img/bleeding.jpg'
+import { useValue } from "../context/itemContext";
+import Add from "../img/addAvatar.png";
+import bleeding from "../img/bleeding.jpg";
 
+const Register = () => {
+  const { appName, handleSubmit, registrationError } = useValue();
 
+  return (
+    <div className="authPage">
+      <div className="authCard">
+        <img src={bleeding} alt="App logo" className="authLogo" />
 
-const Register =() =>{
-    
-    const {appName, handleSubmit, error} = useValue();
+        <h1 className="authTitle">{appName}</h1>
+        <p className="authSubtitle">Create your account</p>
 
-   
+        <form className="authForm" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Display name"
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+          />
 
-    return(
-        <div className="formContainer">
-            <div className="formWrapper">
-                <img src={bleeding} alt="" />
-                <span className="logo">{appName}</span>
-                <span className="title">Register</span>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Display name"/>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="password" />
-                    <input style={{display:"none"}}type="file" id="file"/>
-                    <label htmlFor="file">
-                    <img src={Add} alt="" />
-                    <span> Add an avatar </span>
-                    </label>
-                    <button>Sign Up</button>
-                    {error && <span>Something went wrong</span>}
-                </form>
-                <p>Already have an account? <Link to="/login">Login</Link></p>
+          <input
+            type="file"
+            id="avatar"
+            hidden
+          />
 
-            </div>
+          <label htmlFor="avatar" className="avatarUpload">
+            <img src={Add} alt="Add avatar" />
+            <span>Add an avatar</span>
+          </label>
+
+          <button type="submit">Sign Up</button>
+
+          {registrationError && (
+            <div className="authError">{registrationError}</div>
+          )}
+        </form>
+
+        <div className="authFooter">
+          Already have an account?{" "}
+          <Link to="/login">Login</Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Register;
