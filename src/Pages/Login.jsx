df544eacc27ab4d19b129ac70d3330981e1905b9
Ubair <113605539+Blue-pill-786@users.noreply.github.com>
2026-01-30 05:58:16 +0530

    Login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,36 +1,47 @@
-
 import { useValue } from "../context/itemContext";
 import { Link } from "react-router-dom";
-import bleeding  from '../img/bleeding.jpg'
-const Login =() =>{
-    
-    const { appName, handleSubmitSignIn, error } = useValue();
+import bleeding from "../img/bleeding.jpg";
 
-    const handleSignIn = async (e) => {
-      e.preventDefault();
-      handleSubmitSignIn(e);
-    };
-    return(
-        <div className="formContainer">
-            <div className="formWrapper">
-                <img src={bleeding} alt="" />
-                <span className="logo">{appName}</span>
-                <span className="title">Login</span>
-                
-                <form onSubmit={handleSignIn}>
+const Login = () => {
+  const { appName, handleSubmitSignIn, error } = useValue();
 
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="password" />
-                    
-                   
-                    <button>Sign In</button>
-                </form>
-                {error && <span>{error}</span>}
-                <p>Don't have an account? <Link to="/register">Register</Link></p>
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    handleSubmitSignIn(e);
+  };
 
-            </div>
-             </div>
-    )
-}
+  return (
+    <div className="authPage">
+      <div className="authCard">
+        <img src={bleeding} alt="App icon" className="authLogo" />
+
+        <h1 className="authTitle">{appName}</h1>
+        <p className="authSubtitle">Sign in to continue</p>
+
+        <form onSubmit={handleSignIn} className="authForm">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+          />
+
+          <button type="submit">Sign In</button>
+
+          {error && <span className="authError">{error}</span>}
+        </form>
+
+        <p className="authFooter">
+          Don’t have an account?{" "}
+          <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
